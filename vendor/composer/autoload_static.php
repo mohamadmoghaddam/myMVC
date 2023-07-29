d324697c57fbb75fe7bacde5e701db65b203d205
mohamadreza <mohamadreza.ghasemi1383@gmail.com>
2023-07-29 13:54:21 +0430

    404 error solved

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,6 +11,10 @@ class ComposerStaticInit1212a30546c78c3108d526c5d48d4f3a
         array (
             'Core\\' => 5,
         ),
+        'B' => 
+        array (
+            'Base\\' => 5,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -18,10 +22,21 @@ class ComposerStaticInit1212a30546c78c3108d526c5d48d4f3a
         array (
             0 => __DIR__ . '/../..' . '/app/Core',
         ),
+        'Base\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app/Base',
+        ),
     );
 
     public static $classMap = array (
+        'Base\\Config\\Database' => __DIR__ . '/../..' . '/app/Base/Config/Database.php',
+        'Base\\Controllers\\DefaultController' => __DIR__ . '/../..' . '/app/Base/Controllers/DefaultController.php',
+        'Base\\Models\\User' => __DIR__ . '/../..' . '/app/Base/Models/User.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'Core\\App' => __DIR__ . '/../..' . '/app/Core/App.php',
+        'Core\\BaseController' => __DIR__ . '/../..' . '/app/Core/BaseController.php',
+        'Core\\Interfaces\\ControllerInterface' => __DIR__ . '/../..' . '/app/Core/Interfaces/ControllerInterface.php',
+        'Core\\Interfaces\\UserInterface' => __DIR__ . '/../..' . '/app/Core/Interfaces/UserInterface.php',
         'Core\\Routing' => __DIR__ . '/../..' . '/app/Core/Routing.php',
     );
 
